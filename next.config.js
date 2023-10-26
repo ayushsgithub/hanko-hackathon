b5161577c3eb52d38ded7515497194490f9d455e
Ayush Kumar <ayushchaurasiya78@gmail.com>
2023-10-26 13:42:45 +0530

    nextconfig mod

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
+    async rewrites() {
         return [
           {
-            // matching all API routes
-            source: "/app/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-          }
+            source: '/api/:path*',
+            destination: 'https://hanko-hackathon-seven.vercel.app/',
+          },
         ]
       },
     images: { domains: ['picsum.photos', 'unsplash.com'] },
